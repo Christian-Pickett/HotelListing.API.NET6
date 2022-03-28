13aed988b59e082c72bc178fb335b8b0c4504ec3
Christian-Pickett <christianpickett@protonmail.com>
2022-03-28 12:55:46 -0700

    Added Hotels API Endpoint

--- HotelListing.API/Configurations/MapperConfig.cs
+++ HotelListing.API/Configurations/MapperConfig.cs
@@ -5,6 +5,10 @@ using HotelListing.API.Models.Hotel;
 
 namespace HotelListing.API.Configurations
 {
+    // in order to prevent security issues with users sending Unique IDs or other unneeded information, this mapping class
+    // creates a mapping between the database context and a Data Transfer Object (DTO). The DTO holds only the relevant information
+    // needed to perform the API call in question. For example, when making a HTTPS POST request, the user would be creating a record,
+    // and only needs to send relevant information (E.G. the name of the record), not the ID of the record to be created.
     public class MapperConfig : Profile
     {
         public MapperConfig()
@@ -15,6 +19,7 @@ namespace HotelListing.API.Configurations
             CreateMap<Country, UpdateCountryDto>().ReverseMap();
 
             CreateMap<Hotel, HotelDto>().ReverseMap();
+            CreateMap<Hotel, CreateHotelDto>().ReverseMap();
         }
     }
 }
